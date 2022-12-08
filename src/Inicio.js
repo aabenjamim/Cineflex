dc45461101443ebbe55b1d24623457137d12a252
aabenjamim <alanebenjamimdossantos@gmail.com>
2022-12-08 11:56:54 -0300

    versão com componentização

---
+++ src/Inicio.js
@@ -0,0 +1,5 @@
+export default function Inicio(){
+    return(
+        <h1>Selecione o filme</h1>git remote add origin https://github.com/aabenjamim/projeto10-cineflex.git
+    )
+}
---
+++ src/Topo.js
@@ -0,0 +1,24 @@
+import styled from "styled-components"
+
+export default function Topo(){
+    return(
+        <BarraTopo>
+            <p>CINEFLEX</p>
+        </BarraTopo>
+    )
+}
+
+const BarraTopo = styled.div`
+font-family: 'Roboto', sans-serif;
+width: 100%;
+height: 67px;
+background-color: #C3CFD9;
+color: #E8833A;
+font-size: 34px;
+line-height: 39px;
+display: flex;
+justify-content: center;
+align-items: center;
+font-weight: 400;
+;
+`
